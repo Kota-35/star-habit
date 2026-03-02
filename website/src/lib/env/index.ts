bb2c011ee637ac033dcba2fa1a69fd9f0ebe3ced
Kota-35 <ishikuro6.2@gmail.com>
2026-03-02 15:41:20 +0900

    refactor(website): 環境変数の管理を@t3-oss/env-nextjsに移行
    - 環境変数のスキーマと解析ロジックを削除し、@t3-oss/env-nextjsを使用して新しい環境変数管理を実装
    - クライアントとサーバーの環境変数を統合し、型安全性を向上させるためにzodを活用

--- website/src/lib/env/index.ts
+++ website/src/lib/env/index.ts
@@ -1,2 +1,65 @@
-export { env } from "./buildEnv";
-export type { Env } from "./envSchema";
+import { createEnv } from "@t3-oss/env-nextjs";
+import z from "zod";
+
+export const env = createEnv({
+  client: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: z
+      .string()
+      .trim()
+      .min(1, "required")
+      .describe("Firebase API Key"),
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z
+      .string()
+      .trim()
+      .min(1, "required")
+      .describe("Firebase Auth ドメイン"),
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: z
+      .string()
+      .trim()
+      .min(1, "required")
+      .describe("Firebase プロジェクト ID"),
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z
+      .string()
+      .trim()
+      .min(1, "required")
+      .describe("Firebase Storage バケット"),
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z
+      .string()
+      .trim()
+      .min(1, "required")
+      .describe("Firebase Cloud Messaging の Sender ID"),
+    NEXT_PUBLIC_FIREBASE_APP_ID: z
+      .string()
+      .trim()
+      .min(1, "required")
+      .describe("Firebase アプリ ID"),
+    NEXT_PUBLIC_SERVER_ORIGIN: z.url().describe("サーバーのURL"),
+  },
+
+  server: {
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
+    APP_ENV: z
+      .enum(["development", "staging", "test", "production"])
+      .default("development"),
+  },
+
+  runtimeEnv: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
+      process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    NEXT_PUBLIC_SERVER_ORIGIN: process.env.NEXT_PUBLIC_SERVER_ORIGIN,
+    NODE_ENV: process.env.NODE_ENV,
+    APP_ENV: process.env.APP_ENV,
+  },
+});
+
+export type Env = typeof env;
