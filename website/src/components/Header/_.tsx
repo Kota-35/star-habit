8eaa8b131c1c23510af8f08f1d750813614011c1
Kota-35 <ishikuro6.2@gmail.com>
2026-03-03 23:49:46 +0900

    feat(header): 新しいHeaderコンポーネントを追加
    - Headerコンポーネントを作成し、基本的なレイアウトとスタイルを実装
    - 追加のエクスポート用にindex.tsファイルを作成

---
+++ website/src/components/Header/_.tsx
@@ -0,0 +1,10 @@
+import { Star, User } from "lucide-react";
+import { Button } from "../ui/button";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0 z-50 shrink-0 border-gray-200 border-b bg-white shadow-sm">
+      <div className="flex h-10 items-center justify-end px-6"></div>
+    </header>
+  );
+};
