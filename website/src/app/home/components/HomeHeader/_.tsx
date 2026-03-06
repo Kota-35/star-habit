d19a1509ca27be009efb300c27c5d8803669a4ce
Kota-35 <ishikuro6.2@gmail.com>
2026-03-06 16:16:38 +0900

    feat: /home の Header UIを作成

---
+++ website/src/app/home/components/HomeHeader/_.tsx
@@ -0,0 +1,10 @@
+export const HomeHeader = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">お帰りなさい、○○さん</h1>
+      <p className="mt-2 text-blue-700">
+        今日の振り返りをSTAR形式で記録して、成長を可視化しましょう。
+      </p>
+    </div>
+  );
+};
