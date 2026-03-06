8b73c0f1f1469101cda4a11a99f4725883d4fccd
Kota-35 <ishikuro6.2@gmail.com>
2026-03-06 16:46:18 +0900

    feat: Home のUIを作成

--- website/src/app/home/page.tsx
+++ website/src/app/home/page.tsx
@@ -1,7 +1,9 @@
+import { HomeHeader } from "./components/HomeHeader";
+
 const HomePage = () => {
   return (
-    <div className="p-6">
-      <h1 className="font-semibold text-xl">Home</h1>
+    <div className="p-10">
+      <HomeHeader />
     </div>
   );
 };
